84e00384a92f8467f1bfc63f2247fcf9b65d7a74
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-07 09:34:03 +0300

    generic repository

---
+++ DataAccessLayer/Concrete/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract.Base;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository
+{
+    internal class GenericRepository<T> : IGenericDal<T> where T : class, new()
+    {
+
+        public void Delete(T t)
+        {
+            using var context = new ETicaretContext();
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var context =new ETicaretContext();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var context = new ETicaretContext();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            using var context = new ETicaretContext();
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new ETicaretContext();
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
